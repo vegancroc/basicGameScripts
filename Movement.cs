ee89ae9e99ea6ba45c89a33af5c312138d0d2055
croc <69516460+vegancroc@users.noreply.github.com>
2021-02-10 16:26:12 +0300

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float jumpSpeed;
+
+    bool jumpKeyPressed;
+
+    float horizontalMove;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyPressed = true;
+        }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        if (jumpKeyPressed)
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            jumpKeyPressed = false;
+        }
+
+        rb.velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, rb.velocity.y);
+    }
+}
